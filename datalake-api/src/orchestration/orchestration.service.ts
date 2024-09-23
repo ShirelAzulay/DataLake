3aadba586b488e565fcfaa98fb4cb106e0c62273
Shire Azulay <shirel@peech-ai.com>
2024-09-23 16:39:23 +0300

    Change folders

--- datalake-api/src/orchestration/orchestration.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class OrchestrationService {
-  orchestrateWorkflow(workflow: any): void {
-    // Implement your orchestration logic here
-    console.log(`Workflow: ${JSON.stringify(workflow)}`);
-  }
-}
